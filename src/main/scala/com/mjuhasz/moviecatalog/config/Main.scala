55377ddb7b6c0481e9194dc7ccbca8e370d5e18c
Miklos Juhasz <juhasz.miklos.1@gmail.com>
2012-01-28 14:37:00 +0100

    Add environment option.

--- src/main/scala/com/mjuhasz/moviecatalog/config/Main.scala
+++ src/main/scala/com/mjuhasz/moviecatalog/config/Main.scala
@@ -12,12 +12,16 @@ object Main {
       Console.err.println(error)
       sys.exit(1)
     case Right(options) =>
-      configureLogging()
+      System.setProperty("environment", options.environment)
+      configureLogging(options)
       run(options)
   }
 
-  private def configureLogging() {
-    val configFile = new File("config/log4j.properties")
+  private def configureLogging(options: Options) {
+    var configFile = new File("config/log4j-%s.properties".format(options.environment))
+    if (!configFile.exists()) {
+      configFile = new File("config/log4j.properties")
+    }
     PropertyConfigurator.configureAndWatch(configFile.toString);
   }
 
@@ -42,13 +46,14 @@ object Main {
       logger.info("Terminating...")
     })
     server.start()
-    logger.info("Welcome to the Movie Catalog! Available on port %d. Hit CTRL+C to terminate."
-        .format(options.httpPort))
+    logger.info("Welcome to the Movie Catalog running in %s mode, now available on port %d. Hit CTRL+C to terminate."
+        .format(options.environment.toUpperCase(), options.httpPort))
     server.join()
   }
 }
 
 private[config] object Options {
+  val DEFAULT_ENVIRONMENT = "development"
   val DEFAULT_HTTP_PORT = 8080
 
   def parse(args: Array[String]): Either[String, Options] = try {
@@ -60,7 +65,12 @@ private[config] object Options {
 
 private[config] class Options(args: Array[String]) {
   private val parser = new ArgotParser(programName = "movie-catalog")
+
+  private val environmentOption = parser.option[String](List("e", "environment"), "ENVIRONMENT", "The configuration environment. Default: " + Options.DEFAULT_ENVIRONMENT)
   private val httpPortOption = parser.option[Int](List("p", "port"), "PORT", "The port the movie-catalog listens on for HTTP requests. Default: " + Options.DEFAULT_HTTP_PORT)
+
+  def environment: String = environmentOption.value.getOrElse(Options.DEFAULT_ENVIRONMENT)
   def httpPort: Int = httpPortOption.value.getOrElse(Options.DEFAULT_HTTP_PORT)
+
   parser.parse(args)
 }
