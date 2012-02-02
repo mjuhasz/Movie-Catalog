712926acfaea43bec59ea298e608f2b0fc6ec344
Miklos Juhasz <juhasz.miklos.1@gmail.com>
2012-02-02 22:11:36 +0100

    Added sql schema and notes on generating mediainfo and csv list.

---
+++ sql/movie.sql
@@ -0,0 +1,22 @@
+CREATE TABLE movie (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+title TEXT NOT NULL UNIQUE,
+title_hu TEXT NOT NULL UNIQUE,
+storage_a TEXT NOT NULL,
+storage_b TEXT NOT NULL,
+source TEXT NOT NULL);
+
+CREATE TABLE mediainfo (
+title TEXT PRIMARY KEY,
+size TEXT NOT NULL,
+runtime TEXT NOT NULL,
+audio_lng TEXT NOT NULL,
+subtitle_lng TEXT NOT_NULL,
+resolution TEXT NOT NULL,
+aspect_ratio TEXT NOT NULL,
+framerate TEXT NOT NULL,
+FOREIGN KEY(title) REFERENCES movie(title)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
